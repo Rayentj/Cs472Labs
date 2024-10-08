d1d6c99b113c7dd04d8bc631cd79ef7518176ced
Rayentj <rayen.tajouri@esprit.tn>
2024-10-07 23:37:29 -0500

    Lab7

---
+++ Lab6_Assignment/script.js
@@ -0,0 +1,78 @@
+class Meditation{
+    constructor(second){
+        this.second=second //initialize meditation time
+    }
+    start(){
+        console.log("Start Meditation")
+        this.countdown();
+    }
+
+    countdown(){
+        if(this.second>0){
+            console.log(this.second)
+            this.second--
+            setTimeout(() => {
+                this.countdown()
+            }, 1000);
+        }
+        else{
+            console.log("Rayen Tajouri")
+        }
+    }
+}
+
+
+
+
+const morning_meditation = new Meditation(5)
+morning_meditation.start();
+//console.log(`Start meditation`);
+
+//*****************************Promise*************************** */
+/*const isPrime = (n) => {
+    for (let i = 2, s = Math.sqrt(n); i <= s; i++)
+    if (n % i === 0) return false;
+    return n > 1;
+   };
+*/
+
+const isPrime = (n) => {
+    return new Promise((resolve, reject) => {
+      if (n <= 1) {
+        reject({ prime: false });
+        return;
+      }
+  
+      for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) {
+          reject({ prime: false });
+          return;
+        }
+      }
+  
+      resolve({ prime: true });
+    });
+  };
+
+console.log('start');
+isPrime(10)
+ .then(console.log)
+ .catch(console.error);
+console.log('end');
+
+//**********************Async/await************************ */
+
+const testIsprime = async(n)=>{
+    console.log('start')
+    try{
+        const result = await isPrime(n)
+        console.log(result)
+    }
+    catch(error){
+        console.log(error)
+    }
+    console.log('end')
+}
+
+testIsprime(7)
+testIsprime(10)
